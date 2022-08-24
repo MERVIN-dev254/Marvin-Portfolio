76d764c90d474e617c9dabbdad9144013f7e4dc1
MERVIN-dev254 <o.silverygold@gmail.com>
2022-08-24 08:43:29 +0300

    navigation section complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+  
   title = 'Portfolio';
+  @ViewChild('togglebutton')
+  toggleBtn!: HTMLAnchorElement;
+  @ViewChild('navbarLinks')
+  navbarLinks!: HTMLDivElement;
+
+  //const toggleBtn = document.getElementsByClassName('toggle-button')[0]
+//const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+ngAfterViewInit(){
+  this.toggleBtn.addEventListener(
+    'click', ()=> {
+      this.navbarLinks.classList.toggle('active');}
+  )
+}
+
 }
